dad0ade26acedfeaa45441c7860da7b776c51540
Nicko <n17pang@gmail.com>
2019-11-08 12:38:04 -0500

    Created XCode Project

---
+++ League of Legends TI84 Version/League of Legends TI84 Version/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  League of Legends TI84 Version
+//
+//  Created by Nick Pang on 11/8/19.
+//  Copyright © 2019 Senza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
